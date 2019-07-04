7aef87eaf48ebe5aa8a2695f9d652722ae7e5ec6
felipe ferreira <felipeferreira@localhost.localdomain>
2019-07-04 12:33:52 -0300

    Implemented concat + doc
    - sketch implementation (not tested)
    - documentation in the interface

--- src/utils.c
+++ src/utils.c
@@ -2,4 +2,28 @@
   // Utilities module implementation
   // Guilherme Dantas
 
-  char * concat(int n, char * s, ...) { return 0; }
+  #include <stdlib.h>
+  #include <stdarg.h>
+  #include <string.h>
+  #include "utils.h"
+
+  char * concat(int n, char * s, ...) {
+    va_list va; int size = 1;
+    // Get concatenated string size
+    va_start(va,s);
+    for(int i = 0; i < n; i++) {
+      char * si = va_arg(va,char *);
+      if( si == NULL ) continue;
+      size += strlen(si);
+    } va_end(va);
+    // Concatenate each string
+    char * cs = (char *) malloc(size);
+    if( cs == NULL ) return NULL;
+    va_start(va,s);
+    for(int i = 0; i < n; i++) {
+      char * si = va_arg(va,char *);
+      strcat(cs,si); free(si);
+    } va_end(va);
+    // Return concatenated string
+    return cs;    
+  }
--- src/utils.h
+++ src/utils.h
@@ -2,4 +2,12 @@
   // Utilities module interface
   // Guilherme Dantas
 
+  // Concatenates strings of any size
+  // n - number of strings
+  // s, ... - dynamically allocated strings
+  // > concatenated string, NULL
+  // [!] if any of the strings are NULL,
+  // they'll be simply ignored.
+  // [!] if one of the strings isn't
+  // dynamically allocated, it will crash :)
   char * concat(int n, char * s, ...);
