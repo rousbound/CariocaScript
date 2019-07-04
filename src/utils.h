ff5f413f52f6a1e0fc256cec02f5d9863794d31e
guidanoli <guidanoli@hotmail.com>
2019-07-04 18:56:04 -0300

    Small edit
    - unified define and function of concat with doc

--- src/utils.h
+++ src/utils.h
@@ -5,9 +5,8 @@
 
   #include <stdarg.h>
 
-  #define concat(...) concatfunc((char *)0,__VA_ARGS__,(char *)0)
-
   // Concatenates strings of any size
   // ... - non-null strings
   // > concatenated string, NULL
+  #define concat(...) concatfunc((char *)0,__VA_ARGS__,(char *)0)
   char * concatfunc(char * dummy, ...);
