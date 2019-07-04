2c34ae0b36282dac0a653e7a3e029a6c22b5305b
felipe ferreira <felipeferreira@localhost.localdomain>
2019-07-04 12:11:29 -0300

    Added utilities module (empty)
    - interface
    - implementation

---
+++ src/utils.c
@@ -0,0 +1,5 @@
+
+  // Utilities module implementation
+  // Guilherme Dantas
+
+  char * concat(int n, char * s, ...) { return 0; }
---
+++ src/utils.h
@@ -0,0 +1,5 @@
+
+  // Utilities module interface
+  // Guilherme Dantas
+
+  char * concat(int n, char * s, ...);
